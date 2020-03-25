fb74a3c70bb90a7a2af8fb2a9ca3892a7273b6c0
Xiaoyi Liu <lxy878@gmail.com>
2020-03-25 14:29:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,7 +61,7 @@ def won?(board)
   pre = current_player(board) =='X'? "O" : "X"
   WIN_COMBINATIONS.each do |comb|
     binding.pry
-    if comb[0] == pre and comb[1] == pre and comb[2] == pre
+    if board[comb[0]] == pre and borad[comb[1]] == pre and borad[comb[2]] == pre
       return true
     end
   end
